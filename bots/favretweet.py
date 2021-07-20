866c863842c02743b06dc0c00f8094f834374b97
aliciacalafat <87717029+aliciacalafat@users.noreply.github.com>
2021-07-20 19:10:57 +0200

    Add files via upload

---
+++ bots/favretweet.py
@@ -0,0 +1,57 @@
+
+#============================================================================
+# This bot uses Tweepy stream to actively watch for tweets that contain 
+# certain keywords. For each tweet, if you’re not the tweet author, it will 
+# mark the tweet as liked and then retweet it.                        
+#                                                                  02/01/2021
+#============================================================================
+
+import tweepy
+import logging
+from config import create_api
+import json
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+class FavRetweetListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        logger.info(f"Processing tweet id {tweet.id}")
+        if tweet.in_reply_to_status_id is not None or \
+            tweet.user.id == self.me.id:
+            # This tweet is a reply or I'm its author so, ignore it
+            return
+        if not tweet.favorited:
+            # Mark it as Liked, since we have not done it yet
+            try:
+                tweet.favorite()
+            except Exception as e:
+                logger.error("Error on fav", exc_info=True)
+        if not tweet.retweeted:
+            # Retweet, since we have not retweeted it yet
+            try:
+                tweet.retweet()
+            except Exception as e:
+                logger.error("Error on fav and retweet", exc_info=True)
+
+    def on_error(self, status):
+        logger.error(status)
+
+def main(keywords):
+    api = create_api()
+    tweets_listener = FavRetweetListener(api)
+    stream = tweepy.Stream(api.auth, tweets_listener)
+    stream.filter(track=keywords, languages=["en"])
+
+if __name__ == "__main__":
+    main(["gravitational waves", "black holes"])
+
+#______________________________________________________________________________
+# To run a bot.
+
+# In your directory of the bot, activate python and this file.
+# You can stop the bot using Ctrl-C.
